4adf302f00ebb85bdfc8b13d41468c94473deeac
seescode <seesyong@gmail.com>
2014-05-05 00:08:04 -0400

    scaffolding with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('japaneseHelperApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    
+    	$('#output').html("123 + 456");		
+
+    //kanjiHelper.Helper();
+  });
