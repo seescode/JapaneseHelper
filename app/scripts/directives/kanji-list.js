e87b8514be28c4804d466aa41542bb62695893c7
seescode <seesyong@gmail.com>
2014-05-11 00:26:52 -0400

    Got some kanji outputting for the kanji list page

--- app/scripts/directives/kanji-list.js
+++ app/scripts/directives/kanji-list.js
@@ -5,22 +5,43 @@ angular.module('japaneseHelperApp')
     return {
         restrict: 'A',
         replace: false,
-        template: '<pagination total-items="totalItems" ng-model="currentPage" ng-change="pageChanged()" items-per-page="pageSize"></pagination>',        
+        template: 
+        '<div>' +
+        '  <pagination total-items="totalItems" ng-model="currentPage" ng-change="pageChanged()" items-per-page="pageSize"></pagination>' +
+        '    <ul>{{items}}</ul>' +
+        '  <pagination total-items="totalItems" ng-model="currentPage" ng-change="pageChanged()" items-per-page="pageSize"></pagination>' +
+        '</div>',        
         scope: { 
             pageSize: '@'
         },
         controller: function ($scope) {
         },
         link: function (scope, element, attrs) {
-  			scope.totalItems = 200;
+  			var kanjis = new kanjiHelper.RtkList();
+        
+  			scope.totalItems = kanjis.Length();
   			scope.currentPage = 1;
     
     
   			scope.pageChanged = function() {
+	
+				var pageItems = "";
+				
+				var startIndex = (scope.currentPage - 1) * scope.pageSize + 1;
+				var endIndex = scope.currentPage * scope.pageSize;
+				
+				
+				for (var i=startIndex;i<=endIndex;i++) {
+					pageItems = pageItems + '<li>' + kanjis.GetById(i) + '</li>';
+				}
+				
+				
+				scope.items = pageItems;
 				
   			};
         
-            scope.message = scope.pageSize;
+        
+        
         }
     }
   });
