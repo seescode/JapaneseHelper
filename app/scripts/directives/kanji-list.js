f234e417dc1065d6e517b45c7b02923003443bce
seescode <seesyong@gmail.com>
2014-05-11 00:08:46 -0400

    fixing bower to include angular-bootstrap.  Fixing total item issue for kanji list directive

--- app/scripts/directives/kanji-list.js
+++ app/scripts/directives/kanji-list.js
@@ -5,18 +5,19 @@ angular.module('japaneseHelperApp')
     return {
         restrict: 'A',
         replace: false,
-        template: '<pagination total-items="totalItems" ng-model="currentPage" ng-change="pageChanged()"></pagination>',        
+        template: '<pagination total-items="totalItems" ng-model="currentPage" ng-change="pageChanged()" items-per-page="pageSize"></pagination>',        
         scope: { 
             pageSize: '@'
         },
         controller: function ($scope) {
         },
         link: function (scope, element, attrs) {
-  			scope.totalItems = 64;
-  			scope.currentPage = 5;
+  			scope.totalItems = 200;
+  			scope.currentPage = 1;
+    
     
   			scope.pageChanged = function() {
-
+				
   			};
         
             scope.message = scope.pageSize;
