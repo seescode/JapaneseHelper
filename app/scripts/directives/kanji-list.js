f5d8626136fba24d32ea0606a8cfdadf9af78bfa
seescode <seesyong@gmail.com>
2014-05-13 00:33:36 -0400

    Fixed bugs in kanji-list.js

--- app/scripts/directives/kanji-list.js
+++ app/scripts/directives/kanji-list.js
@@ -8,7 +8,7 @@ angular.module('japaneseHelperApp')
         template: 
         '<div>' +
         '  <pagination total-items="totalItems" ng-model="currentPage" ng-change="pageChanged()" items-per-page="pageSize"></pagination>' +
-        '    <ul>{{items}}</ul>' +
+        '    <ul><li ng-repeat="i in items">{{i}}</li></ul>' +
         '  <pagination total-items="totalItems" ng-model="currentPage" ng-change="pageChanged()" items-per-page="pageSize"></pagination>' +
         '</div>',        
         scope: { 
@@ -21,27 +21,29 @@ angular.module('japaneseHelperApp')
         
   			scope.totalItems = kanjis.Length();
   			scope.currentPage = 1;
+  			
+  			var lastPage = Math.ceil(scope.totalItems / scope.pageSize);
     
     
   			scope.pageChanged = function() {
 	
-				var pageItems = "";
+				var pageItems = [];
 				
 				var startIndex = (scope.currentPage - 1) * scope.pageSize + 1;
 				var endIndex = scope.currentPage * scope.pageSize;
 				
+				if (scope.currentPage === lastPage) {
+					endIndex = scope.totalItems;
+				}				
 				
-				for (var i=startIndex;i<=endIndex;i++) {
-					pageItems = pageItems + '<li>' + kanjis.GetById(i) + '</li>';
+				for (var i=startIndex;i<=endIndex;i++) {					
+					pageItems.push(kanjis.GetById(i));
 				}
-				
-				
-				scope.items = pageItems;
-				
-  			};
-        
-        
-        
+								
+				scope.items = pageItems;				
+  			};        
+  			
+  			scope.pageChanged();
         }
     }
   });
