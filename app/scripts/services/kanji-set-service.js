0f867d770aa5afc88dda163878de065c155d08e0
seescode <seesyong@gmail.com>
2014-06-02 23:13:06 -0400

    made updates to the kanji-set-service

--- app/scripts/services/kanji-set-service.js
+++ app/scripts/services/kanji-set-service.js
@@ -1,24 +1,64 @@
 ﻿'use strict';
 
+/*
+    The way I envision using this is say that you are on level 1 which has kanji 1 to 25.
+    You would then use this service with the first 25 kanji passed into the constructor.
+    You would run getNext on each new kanji until you get to the last one.  In order
+    to get random options I would call getRandomOption.  
+*/
+
 angular.module('japaneseHelperApp').factory('kanjiSetService', function () {
 
     return function (kanjiItemsArray) {
         this.items = kanjiItemsArray;
         this.shuffledItems = _.shuffle(angular.copy(this.items));
+        this.nextIndex = -1;
 
-        this.popUniqueRandom = function (item) {
+        //Call this method to get the next item in the list.  
+        this.getNext = function () {
 
-            if (this.shuffledItems.length <= 0) {
+            if (this.nextIndex + 1 >= this.items.length) {
                 throw {
-                    error: "Cannot pop unique random when length is 0"
+                    error: "Cannot get next: exceeded bounds of the array"
                 };
             }
 
-            return this.shuffledItems.pop();
+            this.nextIndex++;
+
+            return this.items[this.nextIndex];        
         };
 
-        this.length = function () {
-            return this.shuffledItems.length;
+        //Call this method after you have gotten getNext() in order to get possible
+        //random values to choose from.
+        this.getRandomOption = function () {
+
+            if (this.shuffledItems.length <= 0) {
+                throw {
+                    error: "Exhausted all random options."
+                };
+            }
+
+            var randomItem = this.shuffledItems.pop();
+
+            if (this.nextIndex >= 0 && randomItem === this.items[this.nextIndex]) {
+                if (this.shuffledItems.length <= 0) {
+                    throw {
+                        error: "Exhausted all random options."
+                    };
+                }
+
+                randomItem = this.shuffledItems.pop();
+            }
+
+            return randomItem;
+        };
+
+        this.reachedLastElement = function () {
+            if (this.nextIndex + 1 >= this.items.length) {
+                return true;
+            }
+
+            return false;
         };
 
         this.reset = function () {
