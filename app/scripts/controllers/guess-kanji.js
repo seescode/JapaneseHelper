051ddb953db385ceeec05aa184efc04063cdc939
seescode <seesyong@gmail.com>
2014-06-17 23:35:57 -0400

    got the game working and also saved current level to local storage

--- app/scripts/controllers/guess-kanji.js
+++ app/scripts/controllers/guess-kanji.js
@@ -1,32 +1,69 @@
 ﻿'use strict';
 
 angular.module('japaneseHelperApp')
-  .controller('GuessKanjiCtrl', function ($scope, $routeParams, levelGeneratorService) {
+  .controller('GuessKanjiCtrl', function ($scope, $routeParams, $location, localStorageService, levelGeneratorService) {
 
       //Get the current level from the url.
       $scope.level = $routeParams.level
 
+      var currentLevel = localStorageService.get('currentLevel');
+
+      if (currentLevel === null) {
+          localStorageService.set('currentLevel', '1');
+      }
+
+      if ($scope.level == 0) {
+          $scope.level = localStorageService.get('currentLevel');
+      }      
 
       // Need to refactor and put this into the game logic service
       var kanjiSet = levelGeneratorService.generate($scope.level);
-      var options = [];
 
-      options.push(kanjiSet.getNext());
-      options.push(kanjiSet.getRandomOption());
-      options.push(kanjiSet.getRandomOption());
-      options.push(kanjiSet.getRandomOption());
+      function populateItems() {
 
-      $scope.questionText = options[0];
+          var options = [];
+          var questionText = kanjiSet.getNext();
+          options.push(questionText);
+          options.push(kanjiSet.getRandomOption());
+          options.push(kanjiSet.getRandomOption());
+          options.push(kanjiSet.getRandomOption());
+          options = _.shuffle(options);
 
-      options = _.shuffle(options);
+          $scope.questionText = questionText;
+          $scope.answer1 = options[0];
+          $scope.answer2 = options[1];
+          $scope.answer3 = options[2];
+          $scope.answer4 = options[3];
+      }
 
-      $scope.answer1 = options[0];
-      $scope.answer2 = options[1];
-      $scope.answer3 = options[2];
-      $scope.answer4 = options[3];
+      $scope.onAnswerOptionClick = function (kanjiItem) {
 
-      $scope.alertTest = function () {
-          alert(123);
+          if (kanjiItem.equals($scope.questionText)) {
+              if (kanjiSet.reachedLastElement() === true) {
+                  alert("You win! Try the next level");
+
+                  //increment the level
+                  var lvl = parseFloat($scope.level);
+                  lvl++;
+                  $scope.level = String(lvl);
+
+                  //save the level
+                  localStorageService.set('currentLevel', lvl);
+
+                  kanjiSet = levelGeneratorService.generate($scope.level);
+
+
+              }
+
+              populateItems();
+          }
+          else {
+              $location.url('/guess-kanji-level-select');
+          }
+
       };
+
+     
+      populateItems();
 
   });
