359a746f698d0d6b0487a26ed93074e5b2278535
seescode <seesyong@gmail.com>
2015-01-01 20:56:25 -0500

    Removing unused code and adding solution files

--- www/js/controllers/guess-kanji-level-select.js
+++ www/js/controllers/guess-kanji-level-select.js
@@ -12,20 +12,4 @@ angular.module('japaneseHelperApp')
               $scope.currentLevel = 1;
           }
       });
-
-      var totalLevels = Math.ceil(kanjiHelper.RtkList.Length() / constantsService.KANJI_PER_LEVEL);
-
-      $scope.firstHalfLevels = [];
-      $scope.lastHalfLevels = [];
-
-      var i;
-      for (i = 1; i <= totalLevels / 2; i++) {
-          $scope.firstHalfLevels.push(i);
-      }
-
-      var j;
-      for (j = i; j <= totalLevels; j++) {
-          $scope.lastHalfLevels.push(j);
-      }
-
   });
