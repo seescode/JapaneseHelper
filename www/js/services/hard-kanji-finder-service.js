7b29e59af041c498906f0c0a3aa9c9c7db30f5e0
seescode <seesyong@gmail.com>
2015-01-10 22:33:19 -0500

    wrote some unit tests TDD style

---
+++ www/js/services/hard-kanji-finder-service.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+angular.module('japaneseHelperApp').factory('hardKanjiFinderService', function ($localForage, kanjiItemService) {
+
+    var find = function (number) {
+
+        var hardKanjiArray = [];
+
+        if (number <= 0) {
+            return hardKanjiArray;
+        }
+ 
+        //var item = new kanjiItemService(kanji, keyword);
+
+        hardKanjiArray.push(new kanjiItemService('hi', 'fire'));
+        
+
+        return hardKanjiArray;
+    },
+
+    determineRtkRange = function (level) {
+
+    };
+
+
+    return {
+        find: find
+    };
+});
